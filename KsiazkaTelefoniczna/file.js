bc68d8d4a86c63f8f0cfdabcc528cf6fe4f980d0
Arturro <arturro_1996@o2.pl>
2024-01-08 23:24:09 +0100

    Ksiazka Koniec

--- KsiazkaTelefoniczna/file.js
+++ KsiazkaTelefoniczna/file.js
@@ -7,3 +7,45 @@
 // sprawdź czy numer telefonu ma poprawny format (dla uproszczenia: ma mieć 9 znaków)
 // sprawdź czy nazwa kontaktu ma poprawny format (dla uproszczenia: conajmniej 3 znaki)
 // nie modyfikuj pliku index.html
+const ksiazka = []
+document.getElementById("add-contact").addEventListener("click", dodajKontakt)
+document.getElementById("search").addEventListener("change", filtrujKsiazke)
+function dodajKontakt(){
+    const nazwa = document.getElementById("name").value
+    const phoneNumber = document.getElementById("phoneNumber").value
+    const regex = /^[0-9]+$/;
+
+    if(nazwa.length < 3){
+        alert("Podana nazwa jest za krotka! Minimum 3 znaki.")
+        return
+    }
+    if(!regex.test(phoneNumber) || phoneNumber.length !== 9){
+        alert("Numer telefonu musi skladac sie z 9 cyfr!")
+        return
+    }
+    if(ksiazka.find(e => e.nazwa === nazwa))
+    {
+        alert("Podany uzytkownik juz istnieje!")
+        return
+    }
+    ksiazka.push({nazwa: nazwa, numer: phoneNumber})
+    wyswietlKsiazke(ksiazka)
+}
+
+function wyswietlKsiazke(ksiazka){
+    const lista = document.getElementById("contactsList")
+    lista.innerHTML=''
+    ksiazka.forEach(e => {
+        let kontakt = document.createElement("p")
+        kontakt.textContent = `Nazwa: ${e.nazwa} - Number: ${e.numer}`
+        lista.appendChild(kontakt)
+    })
+}
+
+function filtrujKsiazke(){
+    let szukana = document.getElementById("search").value
+    let regex = new RegExp(szukana)
+    wyswietlKsiazke(ksiazka.filter(e => {
+        return (regex.test(e.nazwa) || regex.test(e.numer))
+    }))
+}
