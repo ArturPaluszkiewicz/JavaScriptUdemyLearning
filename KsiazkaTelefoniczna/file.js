631389dfea8d55bb5e9db28eb9b6707d6d5debc4
Arturro <arturro_1996@o2.pl>
2024-01-08 19:54:37 +0100

    Ksiazka telefoniczna - poczatek zadania

---
+++ KsiazkaTelefoniczna/file.js
@@ -0,0 +1,9 @@
+// projekt książka telefoniczna:
+// utwórz aplikację która będzie przechowywać kontakty w pamięci
+// aplikacja ma mieć możliwość dodawania nowych kontaktów, usuwania kontaktów
+// wyświetlania wszystkich kontaktów
+// wyszukiwania kontaktów po nazwie lub numerze telefonu
+// przy dodawanu kontaktu sprawdź czy taki kontakt już istnieje - jeżeli tak to wyświetl odpowiedni komunikat
+// sprawdź czy numer telefonu ma poprawny format (dla uproszczenia: ma mieć 9 znaków)
+// sprawdź czy nazwa kontaktu ma poprawny format (dla uproszczenia: conajmniej 3 znaki)
+// nie modyfikuj pliku index.html
