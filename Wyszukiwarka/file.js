fb0cb79c4f29e3a03aff5b22402cf42f0d121854
Arturro <arturro_1996@o2.pl>
2024-01-11 22:52:07 +0100

    Zadanie Wyszukiwarka - poczatek

---
+++ Wyszukiwarka/file.js
@@ -0,0 +1,10 @@
+// Utwórz prostą aplikację umożliwiającą użytkownikowi wygodne przeszukiwanie treści i śledzenie historii jego ostatnich wyszukiwań.
+// Funkcje Aplikacji:
+// 	• Wyszukiwanie:
+// 		○ Użytkownik może wprowadzić wyszukiwanie w dostępnym polu tekstowym.
+// 		○ Po naciśnięciu przycisku "Szukaj", wprowadzone wyszukiwanie jest zapisywane.
+// 	• Historia Wyszukiwań:
+// 		○ Aplikacja przechowuje historię ostatnich wyszukiwań, nawet po ponownym otwarciu przeglądarki.
+// 		○ Historia jest wyświetlana poniżej formularza, aby użytkownik mógł łatwo przejrzeć poprzednie wyszukiwania.
+// 	• Czyszczenie Historii:
+// Dla wygody użytkownika, istnieje przycisk "Wyczyść historię", który usuwa całą historię wyszukiwań.
