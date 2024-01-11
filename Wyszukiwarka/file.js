f08e8d88e5fdea503a2dc00c479b96c016df0db2
Arturro <arturro_1996@o2.pl>
2024-01-11 23:23:25 +0100

    Zadanie Wyszukiwarka - koniec

--- Wyszukiwarka/file.js
+++ Wyszukiwarka/file.js
@@ -8,3 +8,35 @@
 // 		○ Historia jest wyświetlana poniżej formularza, aby użytkownik mógł łatwo przejrzeć poprzednie wyszukiwania.
 // 	• Czyszczenie Historii:
 // Dla wygody użytkownika, istnieje przycisk "Wyczyść historię", który usuwa całą historię wyszukiwań.
+
+let szukane = []
+if(localStorage.getItem("historia")){
+    szukane = JSON.parse(localStorage.getItem("historia"))
+}
+wyswietlHistorie()
+
+function search(){
+    let szukana = document.getElementById("searchInput").value
+    if(szukana){
+        szukane.unshift(szukana)
+    }
+    localStorage.setItem("historia",JSON.stringify(szukane))
+    wyswietlHistorie()
+    document.getElementById("searchInput").value = ''
+}
+
+function clearHistory(){
+    localStorage.removeItem("historia")
+    szukane = []
+    wyswietlHistorie()
+}
+
+function wyswietlHistorie(){
+    let lista = document.getElementById("recentSearches")
+    lista.innerHTML = ''
+    for(let e of szukane){
+        let nowyElement = document.createElement("div")
+        nowyElement.textContent = e
+        lista.appendChild(nowyElement)
+    }
+}
