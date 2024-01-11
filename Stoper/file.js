0a86ebe11898c771673f848f6b6094e17fa2337f
Arturro <arturro_1996@o2.pl>
2024-01-11 17:17:36 +0100

    Funkcje DodajCzas, zresetuj czas oraz wyswietl czas

---
+++ Stoper/file.js
@@ -0,0 +1,39 @@
+let sekundy = 15
+let minuty = 10
+let godziny = 11
+
+
+function resetZegara(){
+    sekundy = 0
+    minuty = 0
+    godziny = 0
+    ustawCzas()
+}
+
+function ustawCzas(){
+    let godzinaJakoString = (godziny < 10) ? `0${godziny}` : `${godziny}`
+    let minutyJakoString = (minuty < 10) ? `0${minuty}` : `${minuty}`
+    let sekundyJakoString = (sekundy < 10) ? `0${sekundy}` : `${sekundy}`
+    document.getElementById("timer").textContent =`${godzinaJakoString}:${minutyJakoString}:${sekundyJakoString}`
+}
+
+function dodajCzas(x){
+    sekundy = sekundy + x
+    if(sekundy>59){
+        sekundy -= 60
+        minuty++
+    }
+    if(minuty>59){
+        minuty -= 60
+        godziny++
+    }
+    if(godziny>23){
+        resetZegara()
+    }
+    ustawCzas()
+}
+
+setTimeout(ustawCzas,3000)
+setInterval(()=>{
+    dodajCzas(1)
+},1000)
