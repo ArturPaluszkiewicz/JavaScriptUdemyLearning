0aa1f59d17c408b4463168907f96daa9d9c43894
Arturro <arturro_1996@o2.pl>
2024-01-14 21:34:09 +0100

    Zmiana strony wyswietlanych uzytkownikow

--- Paginacja/file.js
+++ Paginacja/file.js
@@ -15,13 +15,33 @@ window.onload = () => {
             nowyHTML += `<tr><td>${i.id}</td><td>${i.name}</td><td>${i.email}</td><td>${i.address.street}</td><td>${i.address.city}</td><td>${i.address.zipCode}</td></tr>`
         }
         tabela.innerHTML = `${nowyHTML}`
+        wyswietlIloscStron(data.totalItems)
     } 
+    function wyswietlIloscStron(iloscWszystkichElementow){
+        let iloscStron = Math.ceil(iloscWszystkichElementow/iloscElementow)
+        let iloscStronElement = document.getElementById("pagination")
+        iloscStronElement.innerHTML = ''
+        for(let i = 1;i<=iloscStron;i++){
+            let nowyElementLi = document.createElement("li")
+            nowyElementLi.addEventListener("click", (event) => {
+                ustawStrone(event.target.textContent)
+            })
+            if(i == strona){
+                nowyElementLi.innerHTML = `<li class="page-item"><a class="page-link active" href="#">${i}</a></li>`
+            }else{
+                nowyElementLi.innerHTML = `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`
+            }
+            iloscStronElement.appendChild(nowyElementLi)
+        }
+    }
     
     function ustawIloscElementow(value){
         iloscElementow = value
+        strona = 1
         wyswietlTabele()
     }
-    function ustawStrone(){
+    function ustawStrone(value){
+        strona = value
         wyswietlTabele()
     }
     function ustawWyszukiwanie(){
@@ -31,5 +51,13 @@ window.onload = () => {
     wielkoscStrony.addEventListener("change", (event) => {
         ustawIloscElementow(wielkoscStrony.options[wielkoscStrony.options.selectedIndex].value)
     })
+    let stronicowanie = document.getElementById("pagination")
+    let wzorzec = /\bactive\b/
+    for(let e of stronicowanie.children){
+        if(wzorzec.test(e.innerHTML)){
+            console.log(e.children[0].textContent)
+        }
+    }
+    console.log(stronicowanie.children)
     
 }
